85e353048b868bcf38282f907642040bd99b5a89
Brandon L. Byrd <93562991+bbyrd2021@users.noreply.github.com>
2026-05-20 13:54:15 -0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        def my_intuition(nums, k):
+            count_map = defaultdict(int)
+
+            for i in nums:
+                count_map[i] += 1
+
+            counts = count_map.items()
+
+            sorted_counts  = sorted(counts, key=lambda x:x[1], reverse=True)
+
+            res = []
+            return [j[0] for j in sorted_counts[:k]]
+
+        def my_optimal(nums, k):
+            count_map = defaultdict(int)
+            count_heap = heapq
+
+            for i in nums:
+                count_map[i] += 1
+            counts = count_map.items()
+            top_k = heapq.nlargest(k, counts, key=lambda x: x[1])
+
+            return [j[0] for j in top_k]
+
+        # return my_intuition(nums, k)
+        return my_optimal(nums, k)
+
+
+
+
+
+        
