866234be1c4fe29a39b5a34fc8aa6b6f4ea61006
Brandon L. Byrd <93562991+bbyrd2021@users.noreply.github.com>
2026-07-19 16:18:32 -0500

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        int result;
+
+        for (string& token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int num2 = stk.top(); stk.pop();
+                int num1 = stk.top(); stk.pop();
+
+                if (token == "+"){
+                    stk.push(num1 + num2);
+                } else if (token == "-") {
+                    stk.push(num1 - num2);
+                } else if (token == "*") {
+                    stk.push(num1 * num2);
+                } else {
+                    stk.push(num1 / num2); // C++ truncates toward zero for ints
+                }
+            } else {
+                stk.push(stoi(token));
+            }
+        }
+
+        return stk.top();
+    }
+};
